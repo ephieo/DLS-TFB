5e6651d3d438c7a8a6e634b736c132763e430870
Jihyun-Jang <jhjang742@gmail.com>
2021-01-09 13:33:58 +0000

    Making signup function
    Co-authored-by: ephieo <ephieo@rocketmail.com>

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,4 +1,6 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+//importing auth function that was initialized in firebase.js
+import { auth } from './../database/firebase'
 
 //creating context using React.createContext();
 const AuthContext = React.createContext();
@@ -11,9 +13,26 @@ export function AuthProvider({ children }) {
   //creating state for users it starts empty becuase we have no users.
   const [currentUser, setCurrentUser] = useState();
 
+  function signup (email,password){
+     // createUserWithEmailAndPassword is a function from firebase creating a new signup 
+    return auth.createUserWithEmailAndPassword(email,password);
+    
+  }
+  //onAuthStateChanged is triggered when a user signs in and signs out,
+  // and returns an unsubscribe to stop that function from running
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setCurrentUser(user)
+    })
+    
+    return unsubscribe;    
+  }, [])
+
   //passing the value of the current user into the context provider below.
   const value = {
     currentUser,
+    signup
   };
 
   return (

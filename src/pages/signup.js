45335eada340841af6843cf772b75d9a81311071
ephie oyedoh <ephieo@rocketmail.com>
2021-01-09 14:23:12 +0000

    working on signup route
    connecting to firebase

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,40 +1,39 @@
 import React, { useRef, useState } from 'react';
 import { Form, Input, Label, SubmitButton } from './../components/Form';
 import { Link } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext'
+import { useAuth } from '../contexts/AuthContext';
 
 function Signup() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const confirmPasswordRef = useRef();
-  //calling and intializing useAuth() to use the signup function to create new users and call 
+  //calling and intializing useAuth() to use the signup function to create new users and call
   //the unsubsribe function in the useffect()
-  const {signup, currentUser} = useAuth();
-  const [ error, setError ] = useState('')
-  const [ loading, setLoading ] = useState(false)
+  const { signup, currentUser } = useAuth();
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
   async function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    if(passwordRef.current.value !== confirmPasswordRef.current.value) {
-      return setError('Passwords do not match')
+    if (passwordRef.current.value !== confirmPasswordRef.current.value) {
+      return setError('Passwords do not match');
     }
-    try{
-      setError('')
-      setLoading(true)
-      await signup(emailRef.current.value, passwordRef.current.value)
+    try {
+      setError('');
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
     } catch {
-      
       setError('Failed to create an account');
     }
-    setLoading(false)
+    setLoading(false);
   }
-  
+
   return (
     <div>
       <h1>Sign up</h1>
-      {currentUser ? JSON.stringify(currentUser.email): <h1>no users yet</h1>}
-      {error? alert(error): null}
+      {currentUser ? JSON.stringify(currentUser.email) : <h1>no users yet</h1>}
+      {error ? alert(error) : null}
       <Form onSubmit={handleSubmit}>
         <Label htmlFor="email">Email:</Label>
         <br />
@@ -46,15 +45,22 @@ function Signup() {
           id="password"
           name="password"
           ref={passwordRef}
+          minLength="7"
+          required
         />
         <Input
           type="password"
           id="confirmPassword"
           name="confirmPassword"
           ref={confirmPasswordRef}
+          minLength="7"
+          required
         />
-        {!loading ?  <SubmitButton type="submit" /> : <button>Loading...</button>}
-      
+        {!loading ? (
+          <SubmitButton type="submit" />
+        ) : (
+          <button>Loading...</button>
+        )}
       </Form>
       <h1> Already have an account ? </h1>
       <Link to="/login">

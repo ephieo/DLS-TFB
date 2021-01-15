b1236e9d71d65cb0be5c379f8630555056ee4c93
Jihyun-Jang <jhjang742@gmail.com>
2021-01-15 19:59:23 +0000

    Fix updateScore function

--- src/database/queries.js
+++ src/database/queries.js
@@ -4,26 +4,30 @@ import 'firebase/firestore';
 
 export function updateScore(uid) {
   const docRef = db.collection('users').doc(uid);
-  let currentScore;
+  
   docRef
     .get()
     .then((doc) => {
-      if (doc.exists) {
-        currentScore += Number(doc.data().score);
+      if (doc.exists) {        
+         return doc.data().score;        
       } else {
         // doc.data() will be undefined in this case
         console.log('No such document!');
       }
     })
-    .then()
+    .then(score => {      
+      return db.collection('users')
+        .doc(uid)
+        .update({ score: score + 100 });
+    })
     .catch((error) => {
       console.log('Error getting document:', error);
     });
 
-  return db
-    .collection('users')
-    .doc(uid)
-    .update({ score: (currentScore += 100) });
+  // return db
+  //   .collection('users')
+  //   .doc(uid)
+  //   .update({ score: (currentScore += 100) });
 }
 
 export function signupDB(uid, email, userName) {
